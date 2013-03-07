a4a860340789ef84839a7316a3aa254548eb755b
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-03-07 21:03:39 +0900

    1-3-3 Improved version

--- 1.chapter.1/first.py
+++ 1.chapter.1/first.py
@@ -9,10 +9,11 @@ first = 0
 other = 0
 
 for i in table.records:
-    if i.birthord == 1:
-        first += 1
-    elif i.birthord <= 10:
-        other += 1
+    if i.outcome == 1:
+        if i.birthord == 1:
+            first += 1
+        else:
+            other += 1
 
 print u' 第一子：', first
 print u' 第二子以降：', other

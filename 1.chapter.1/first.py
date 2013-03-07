49ae76cef263103553d5ab84bb35907ec7080f8d
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-03-07 21:10:37 +0900

    1-3-4

--- 1.chapter.1/first.py
+++ 1.chapter.1/first.py
@@ -5,15 +5,19 @@ import survey
 table = survey.Pregnancies()
 table.ReadRecords()
 
-first = 0
-other = 0
+first_count = 0
+first_prglength = 0
+other_count = 0
+other_prglength = 0
 
 for i in table.records:
     if i.outcome == 1:
         if i.birthord == 1:
-            first += 1
+            first_count += 1
+            first_prglength += i.prglength * 7 * 24
         else:
-            other += 1
+            other_count += 1
+            other_prglength += i.prglength * 7 * 24
 
-print u' 第一子：', first
-print u' 第二子以降：', other
+print u' 第一子妊娠期間の平均：', first_prglength/first_count
+print u' 第二子以降妊娠期間の平均：', other_prglength/other_count

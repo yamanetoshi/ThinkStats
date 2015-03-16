98b81f99ad019cb903b93bce7d443f700c3056b1
YAMANE Toshiaki <yamanetoshi@gmail.com>
2015-03-16 14:29:27 +0900

    fix 1.chapter.1/first.py

--- 1.chapter.1/first.py
+++ 1.chapter.1/first.py
@@ -5,6 +5,10 @@ import survey
 table = survey.Pregnancies()
 table.ReadRecords()
 
+# print u' 妊娠レコードの総数 : ' , len(table.records)
+
+count = 0
+
 first_count = 0
 first_prglength = 0
 other_count = 0
@@ -12,6 +16,7 @@ other_prglength = 0
 
 for i in table.records:
     if i.outcome == 1:
+        count += 1
         if i.birthord == 1:
             first_count += 1
             first_prglength += i.prglength * 7 * 24
@@ -19,5 +24,6 @@ for i in table.records:
             other_count += 1
             other_prglength += i.prglength * 7 * 24
 
+print u' 生児出生数 : ', count
 print u' 第一子妊娠期間の平均：', first_prglength/first_count
 print u' 第二子以降妊娠期間の平均：', other_prglength/other_count
